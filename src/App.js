7a9500eb32ccf31bf1cce7ef6436f1b28692b155
Sang Pham <sangpv.hoangha@gmail.com>
2022-10-30 12:15:45 +0700

    Add demo comment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Comment from './Comment';
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Phạm Sáng
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Phạm Sáng
+                </p>
+                <Comment
+                    date={comment.date}
+                    text={comment.text}
+                    author={comment.author}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
